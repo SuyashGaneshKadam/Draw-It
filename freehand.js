b2e7d511c0c1374e273d5ad848edaae017effd56
Suyash Kadam <suyash.kadam01@ad.infosys.com>
2023-10-28 06:00:51 +0530

    Undo,Redo and Shapes

--- freehand.js
+++ freehand.js
@@ -1,41 +1,96 @@
-const pencil = document.getElementById("pencil");
-let isPencilActive = false;
-let isMainRemoved = false;
+let previousPoint;
 
-const colorSelector = document.getElementById("color-selector");
-colorSelector.addEventListener("change", () => {
-    drawingColor = colorSelector.value ;
-});
+let history = [];
+let historyIndex = -1;
 
-const widthSelector = document.getElementById("width-selector");
-widthSelector.addEventListener("change", () => {
-    strokeWidth = widthSelector.value ;
-});
+canvas.addEventListener("mousedown", onMouseDown);
 
-const opacitySelector = document.getElementById("opacity-selector");
-opacitySelector.addEventListener("change", () => {
-    opacity = opacitySelector.value ;
-});
-
-function onPencilClick() {
-  if (!isMainRemoved) {
-    const main = document.getElementById("main");
-    main.remove();
-    isMainRemoved = true;
+function onMouseDown(event) {
+  if (actions.arrow) {
+    return;
   }
 
-  const sidebar = document.getElementById("sidebar");
-  sidebar.classList.toggle("display");
+  redoArray = [];
 
-  pencil.classList.toggle("button-active");
-  isPencilActive = !isPencilActive;
-  if (isPencilActive) {
-    canvas.style.cursor = "crosshair";
-    canvas.addEventListener("mousedown", onMouseDown);
+  previousPoint = [event.clientX, event.clientY];
+  startIndex = history.length - 1;
+  
+  c.strokeStyle = inputs.color;
+  c.lineWidth = inputs.strokeWidth;
+  c.globalAlpha = inputs.opacity;
+
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mouseup", onMouseUp);
+}
+
+function onMouseMove(e) {
+  let currentPoint = [e.clientX, e.clientY];
+  if (actions.pencil) {
+    drawingFreeHand(currentPoint);
+  } else if (actions.square) {
+    resetToOriginalImage();
+    drawingSquare(currentPoint);
+  } else if (actions.circle) {
+    resetToOriginalImage();
+    drawingCircle(currentPoint);
+  } else if (actions.line) {
+    resetToOriginalImage();
+    drawingLine(currentPoint);
   } else {
-    canvas.style.cursor = "default";
-    canvas.removeEventListener("mousedown", onMouseDown);
+    erasing(currentPoint);
   }
 }
 
-pencil.addEventListener("click", onPencilClick);
+function drawingFreeHand(currentPoint) {
+  c.beginPath();
+  c.moveTo(previousPoint[0], previousPoint[1]);
+  c.lineTo(...currentPoint);
+  c.lineCap = "round";
+  c.lineJoin = "round";
+  c.closePath();
+  c.stroke();
+  previousPoint = currentPoint;
+}
+
+function resetToOriginalImage() {
+  if (startIndex === -1) {
+    c.clearRect(0, 0, canvas.width, canvas.height);
+  } else {
+    c.putImageData(history[startIndex], 0, 0);
+  }
+}
+
+function drawingSquare(currentPoint) {
+  c.beginPath();
+  let width = currentPoint[0] - previousPoint[0];
+  let height = currentPoint[1] - previousPoint[1];
+  c.strokeRect(...previousPoint, width, height);
+}
+
+function drawingCircle(currentPoint){
+  c.beginPath();
+  const radius = Math.sqrt(((currentPoint[0] - previousPoint[0]) ** 2) + ((currentPoint[1] - previousPoint[1]) ** 2));
+  c.arc(...previousPoint, radius, 0, 2 * Math.PI, true);
+  c.stroke();
+}
+
+function drawingLine(currentPoint){
+  c.beginPath();
+  c.moveTo(...previousPoint);
+  c.lineTo(...currentPoint);
+  c.stroke();
+}
+
+function erasing(currentPoint) {
+  let x = inputs.strokeWidth;
+  c.clearRect(...currentPoint, x, x);
+}
+
+function onMouseUp() {
+  history.push(c.getImageData(0, 0, canvas.width, canvas.height));
+  historyIndex++;
+  canvas.removeEventListener("mousemove", onMouseMove);
+  canvas.removeEventListener("mouseup", onMouseUp);
+}
+
+
--- script.js
+++ script.js
@@ -3,28 +3,93 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const c = canvas.getContext("2d");
 
-let previousPoint;
-let drawingColor= "black";
-let strokeWidth = 5;
-let opacity = 1;
+let nav = document.getElementById("nav-middle-top");
+nav.addEventListener("click", onClickNav);
+let isMainRemoved = false;
 
-function onMouseDown(event){
-    previousPoint = [event.clientX, event.clientY];
-    canvas.addEventListener("mousemove",onMouseMove);
-    canvas.addEventListener("mouseup", onMouseUp);
+const topButtons = document.querySelectorAll("#nav-middle-top > button");
+
+const actions = {
+  arrow: true,
+  square: false,
+  circle: false,
+  line: false,
+  pencil: false,
+  eraser: false,
+};
+
+const inputs = {
+  color: "black",
+  strokeWidth: 5,
+  opacity: 1,
+};
+
+function toggleMenu() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.toggle("display");
 }
-function onMouseMove(event){
-    let currentPoint = [event.clientX, event.clientY];
-    c.beginPath();
-    c.moveTo(previousPoint[0],previousPoint[1]);
-    c.lineTo(...currentPoint);
-    c.strokeStyle = drawingColor;
-    c.lineWidth = strokeWidth;
-    c.globalAlpha = opacity;
-    c.stroke();
-    c.closePath();
-    previousPoint = currentPoint;
+
+function onClickNav() {
+  if (!isMainRemoved) {
+    const main = document.getElementById("main");
+    main.remove();
+    isMainRemoved = true;
+  }
 }
-function onMouseUp(){
-    canvas.removeEventListener("mousemove",onMouseMove);
+
+function onInput(currentElement) {
+  const value = currentElement.value;
+  if (currentElement.name === "color") {
+    inputs.color = value;
+  } else if (currentElement.name === "opacity") {
+    inputs.opacity = value;
+  } else {
+    inputs.strokeWidth = parseInt(value);
+  }
+  // console.log(inputs);
 }
+
+function onActionButtonClick(element) {
+  topButtons.forEach((element) => {
+    element.classList.remove("button-active");
+    actions[element.id] = false;
+  });
+  element.classList.add("button-active");
+  actions[element.id] = true;
+
+  if (actions.arrow) {
+    canvas.style.cursor = "default";
+  } else if (actions.eraser) {
+    canvas.style.cursor = "cell";
+  } else {
+    canvas.style.cursor = "crosshair";
+  }
+  // console.log(actions);
+}
+
+function resetCanvas() {
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  history = [];
+  historyIndex = -1;
+}
+
+// function onMouseDown(event) {
+//   previousPoint = [event.clientX, event.clientY];
+//   canvas.addEventListener("mousemove", onMouseMove);
+//   canvas.addEventListener("mouseup", onMouseUp);
+// }
+// function onMouseMove(event) {
+//   let currentPoint = [event.clientX, event.clientY];
+//   c.beginPath();
+//   c.moveTo(previousPoint[0], previousPoint[1]);
+//   c.lineTo(...currentPoint);
+//   c.strokeStyle = drawingColor;
+//   c.lineWidth = strokeWidth;
+//   c.globalAlpha = opacity;
+//   c.closePath();
+//   c.stroke();
+//   previousPoint = currentPoint;
+// }
+// function onMouseUp() {
+//   canvas.removeEventListener("mousemove", onMouseMove);
+// }
